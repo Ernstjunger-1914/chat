3f5aa52eed909bed5e714274c95d4e7f838d995b
김규현 <kgh2703@irlink.co.kr>
2022-03-21 09:57:14 +0900

    1. react-router-dom v6 관련 문제 수정 2. 404 에러 처리

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Join from './components/Join';
 import Chat from './components/Chat';
-import './App.css';
+import NotFound from './components/404';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={Join} />
-        <Route path='/chat' element={Chat} />
+        <Route path='/' element={<Join />} />
+        <Route path='/chat' element={<Chat />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
