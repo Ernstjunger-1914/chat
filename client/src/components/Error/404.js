518e1d91a3941418f2dd7f210ed5f764bea3545d
김규현 <kgh2703@irlink.co.kr>
2022-03-22 11:40:57 +0900

    1. 에러 페이지 구성

--- client/src/components/Error/404.js
+++ client/src/components/Error/404.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './404.css'
 
 function NotFound() {
     return (
-      <h1>Page NotFound</h1>
+      <div className='Container'>
+        <h1>존재하지 않는 페이지입니다.</h1>
+        <Link to='/' className='Gohome'>홈 페이지로 돌아가기</Link>
+      </div>
     );
   }
 
