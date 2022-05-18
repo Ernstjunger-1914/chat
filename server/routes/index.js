cf0d46f97caec74a5c24bb704cff67559b33c863
손소독 <vot768@naver.com>
2022-05-18 21:20:44 +0900

    1. 프로젝트 정리

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.send({ response: "Server is up and running." }).status(200);
-});
-
-module.exports = router;
