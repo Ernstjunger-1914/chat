562b025dc8e51337bd8ecb9a210cd98da094b05c
손소독 <vot768@naver.com>
2022-05-30 21:38:25 +0900

    1. index.jsx 내 렌더링 부분 Root.jsx로 분리

---
+++ src/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './App';
+
+function Root() {
+    return (
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+}
+
+export default Root;
